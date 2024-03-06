25dbeeb5fbbc2455902742ec1cb6c4c430bad21c
Benjamin Quinn <benjamin.quinn@gmail.com>
2024-03-06 17:53:38 -0500

    Changeable chart type

--- frontend-next/src/routes/income-statement/+page.svelte
+++ frontend-next/src/routes/income-statement/+page.svelte
@@ -5,6 +5,9 @@
 	import { onMount } from 'svelte';
 	import bb, { bar } from 'billboard.js';
 
+	let chartEl: HTMLElement | undefined;
+	let chart: bb.Chart | undefined;
+
 	let currencies: string[] = ['USD', 'VACHR', 'IRAUSD'];
 	$: selectedCurrencies = [...currencies];
 
@@ -14,19 +17,28 @@
 	let units = ['At Cost', 'At Market Value', 'Units', 'Converted to USD'];
 	let selectedUnit = 'At Cost';
 
-	let chartEl: HTMLElement | undefined;
+	let chartTypes = [
+		{ value: 'stacked_bars', name: 'Stacked Bars' },
+		{ value: 'single_bars', name: 'Single Bars' }
+	];
+	let activeChartType = 'stacked_bars';
+
+	const chartData: bb.Data = {
+		columns: [
+			['data1', 30, 200, 100, 400, 150, 250],
+			['data2', 130, 100, 140, 200, 150, 50],
+			['data3', 130, -150, 200, 300, -200, 100]
+		],
+		type: bar(),
+	};
+  const chartGroups = [['data1', 'data2', 'data3']];
 
 	onMount(() => {
-		var chart = bb.generate({
+		chart = bb.generate({
 			data: {
-				columns: [
-					['data1', 30, 200, 100, 400, 150, 250],
-					['data2', 130, 100, 140, 200, 150, 50],
-					['data3', 130, -150, 200, 300, -200, 100]
-				],
-				type: bar(),
-				groups: [['data1', 'data2', 'data3']]
-			},
+        ...chartData,
+        groups: chartGroups,
+      },
 			title: {
 				text: 'Income'
 			},
@@ -38,8 +50,45 @@
 			bindto: chartEl
 		});
 
-		return () => chart.destroy();
+		return () => chart?.destroy();
 	});
+
+  // Handle active chart type change.
+	$: if (chart) {
+    chart.destroy();
+		if (activeChartType == 'stacked_bars') {
+      chart = bb.generate({
+        data: {
+          ...chartData,
+          groups: chartGroups,
+        },
+        title: {
+          text: 'Income'
+        },
+        bar: {
+          width: {
+            ratio: 0.5
+          }
+        },
+        bindto: chartEl
+      })
+		} else {
+      chart = bb.generate({
+        data: {
+          ...chartData,
+        },
+        title: {
+          text: 'Income'
+        },
+        bar: {
+          width: {
+            ratio: 0.5
+          }
+        },
+        bindto: chartEl
+      })
+		}
+	}
 </script>
 
 <PageContent>
@@ -47,10 +96,10 @@
 
 	<div class="mt-2 flex flex-col">
 		<!-- Heading -->
-    <div class="h-64 w-full" bind:this={chartEl}></div>
+		<div class="h-64 w-full" bind:this={chartEl}></div>
 
-		<div class="grid grid-cols-1 space-y-2 lg:space-y-0 lg:grid-cols-2">
-      <!-- Currency selection -->
+		<div class="grid grid-cols-1 space-y-2 lg:grid-cols-2 lg:space-y-0">
+			<!-- Currency selection -->
 			<Select.Root
 				multiple
 				selected={selectedCurrencies.map((s) => {
@@ -80,10 +129,21 @@
 
 			<!-- Right side of header -->
 			<div class="flex flex-row space-x-2">
-				<Tabs.Root value="account" class="w-64">
+				<Tabs.Root
+					class="w-64"
+					onValueChange={(typ) => {
+						if (typ) {
+							activeChartType = typ;
+						} else {
+							activeChartType = 'stacked_bars';
+						}
+					}}
+					value={activeChartType}
+				>
 					<Tabs.List class="grid w-full grid-cols-2">
-						<Tabs.Trigger value="account">Stacked Bars</Tabs.Trigger>
-						<Tabs.Trigger value="password">Single Bars</Tabs.Trigger>
+						{#each chartTypes as chartType}
+							<Tabs.Trigger value={chartType.value}>{chartType.name}</Tabs.Trigger>
+						{/each}
 					</Tabs.List>
 				</Tabs.Root>
 
